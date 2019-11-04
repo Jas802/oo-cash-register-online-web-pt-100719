67d2605b5f8884bc0a8921adeb125e49be133754
Jas Spencer <jas.spencer414@gmail.com>
2019-11-04 00:50:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
 
-attr_accessor :itmes, :discount, :total, :last_transaction
+attr_accessor :items, :discount, :total, :last_transaction
 
 def initialize(discount=0)
   @total = 0
